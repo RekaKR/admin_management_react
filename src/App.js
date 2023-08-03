ec9c871097a1839341078064d7be2f9b8870a9c6
Reka Kupsza <khusire@gmail.com>
2023-08-03 22:06:55 +0200

    create react app and add assets

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./style.css"
+import { NewColleauge } from "./NewColleauge";
+
+function App() {
+  return (
+    <div className="App">
+      <NewColleauge />
+    </div>
+  );
+}
+
+export default App;
