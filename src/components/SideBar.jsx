a3f3adc7f83347b809508e5349a3f7da2809ba6f
Reka Kupsza <khusire@gmail.com>
2023-08-04 10:40:09 +0200

    create sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 const SideBar = () => {
   return (
     <div className="side-bar">
@@ -7,14 +9,14 @@ const SideBar = () => {
       </div>
 
       <div className="links">
-        <p>Tasks</p>
-        <p>Divisions</p>
-        <p>Colleagues</p>
-        <p>Users</p>
-        <p>Addresses</p>
-        <p>Routes</p>
-        <p>Logistics</p>
-        <p>Settings</p>
+        <Link to="tasks"><p>Tasks</p></Link>
+        <Link to="divisions"><p>Divisions</p></Link>
+        <Link to="colleagues"><p>Colleagues</p></Link>
+        <Link to="users"><p>Users</p></Link>
+        <Link to="addresses"><p>Addresses</p></Link>
+        <Link to="routes"><p>Routes</p></Link>
+        <Link to="logistics"><p>Logistics</p></Link>
+        <Link to="settings"><p>Settings</p></Link>
       </div>
     </div>
   )
