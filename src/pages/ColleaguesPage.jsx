47f5df0366e4b08df6bf4b9aab1f3978fbe2218a
Reka Kupsza <khusire@gmail.com>
2023-08-04 15:22:25 +0200

    refactor colleagues page

--- src/pages/ColleaguesPage.jsx
+++ src/pages/ColleaguesPage.jsx
@@ -18,79 +18,21 @@ const ColleaguesPage = () => {
       <StatisticsField collegues={collegues} />
 
       <div className="colleagues-list">
-        <div className="text-wrapper-11">Colleagues</div>
+        <h2>Colleagues</h2>
 
         <div className="frame-3">
-          <div className="frame-4">
-            <div className="text-wrapper-12">Name</div>
-            <div className="text-wrapper-13">Email</div>
-            <div className="text-wrapper-13">Phone number</div>
-            <div className="text-wrapper-14">Edit</div>
+          <div className="data-container">
+            <p className="middle-text">Name</p>
+            <p className="long-text">Email</p>
+            <p className="long-text">Phone number</p>
+            <p className="small-text">Edit</p>
           </div>
 
-          <div className="frame-5">
-            <div className="frame-6">
-              <div className="text-wrapper-15">Alex Johnson</div>
-              <div className="text-wrapper-16">alex.johnson@dpd.com</div>
-              <div className="text-wrapper-16">06701234560, 06701234567</div>
-              <img className="edit" alt="edit sign" src="../../assets/edit.png" />
-            </div>
-            {/*
-                <div className="frame-6">
-                  <div className="text-wrapper-15">Jane Smith</div>
-                  <div className="text-wrapper-16">jane.smith@dpd.com</div>
-                  <div className="text-wrapper-16">06701234561</div>
-                  <img className="edit" alt="edit sign" src="../../assets/edit.png" />
-                </div>
-                <div className="frame-6">
-                  <div className="text-wrapper-15">Emily Brown</div>
-                  <div className="text-wrapper-16">emily.brown@dpd.com</div>
-                  <div className="text-wrapper-16">06701234562, 06701234568</div>
-                  <img className="edit" alt="edit sign" src="../../assets/edit.png" />
-                </div>
-                <div className="frame-6">
-                  <div className="text-wrapper-17">Michael Williams</div>
-                  <div className="text-wrapper-18">michael.williams@dpd.com</div>
-                  <div className="text-wrapper-16">06</div>
-                  <img className="edit" alt="edit sign" src="../../assets/edit.png" />
-                </div>
-                <div className="frame-6">
-                  <div className="text-wrapper-15">Sophia Martinez</div>
-                  <div className="text-wrapper-16">sophia.martinez@dpd.com</div>
-                  <div className="text-wrapper-16">06701234564, 06701234569</div>
-                  <img className="edit" alt="edit sign" src="../../assets/edit.png" />
-                </div>
-                <div className="frame-6">
-                  <div className="text-wrapper-17">William Wilson</div>
-                  <div className="text-wrapper-18">william.wilson@dpd.com</div>
-                  <div className="text-wrapper-16">06</div>
-                  <img className="edit" alt="edit sign" src="../../assets/edit.png" />
-                </div>
-                <div className="frame-6">
-                  <div className="text-wrapper-15">James Anderson</div>
-                  <div className="text-wrapper-16">james.anderson@dpd.com</div>
-                  <div className="text-wrapper-16">06701234565</div>
-                  <img className="edit" alt="edit sign" src="../../assets/edit.png" />
-                </div>
-                <div className="frame-6">
-                  <div className="text-wrapper-15">Emma Taylor</div>
-                  <div className="text-wrapper-16">emma.taylor@dpd.com</div>
-                  <div className="text-wrapper-16">06701234566</div>
-                  <img className="edit" alt="edit sign" src="../../assets/edit.png" />
-                </div>
-                <div className="frame-6">
-                  <div className="text-wrapper-17">Emma Wilson</div>
-                  <div className="text-wrapper-18">emma.wilson@dpd.com</div>
-                  <div className="text-wrapper-16">06</div>
-                  <img className="edit" alt="edit sign" src="../../assets/edit.png" />
-                </div>
-                <div className="frame-6">
-                  <div className="text-wrapper-15">Olivia Davis</div>
-                  <div className="text-wrapper-16">olivia.davis@dpd.com</div>
-                  <div className="text-wrapper-16">06701234563</div>
-                  <img className="edit" alt="edit sign" src="../../assets/edit.png" />
-                </div>
-                */}
+          <div className="data-container">
+            <div className="name middle-text">Alex Johnson</div>
+            <div className="data long-text">alex.johnson@dpd.com</div>
+            <div className="data long-text">06701234560, 06701234567</div>
+            <img className="edit" alt="edit sign" src="../../assets/edit.png" />
           </div>
         </div>
       </div>
