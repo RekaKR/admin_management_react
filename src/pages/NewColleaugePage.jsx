2d496f28802fe2c8fc1f8e049f079ab15e7dc0a0
Reka Kupsza <khusire@gmail.com>
2023-08-05 05:57:13 +0200

    update NewColleaugePage with validations & update dummy fetch

--- src/pages/NewColleaugePage.jsx
+++ src/pages/NewColleaugePage.jsx
@@ -1,19 +1,22 @@
 import { useState } from "react"
-import NewColleagueButtons from "../components/assets/NewColleagueButtons";
-import NewColleagueForm from "../components/colleagues/NewColleagueForm";
+import { useNavigate } from "react-router-dom"
+import NewColleagueButtons from "../components/assets/NewColleagueButtons"
+import NewColleagueForm from "../components/colleagues/NewColleagueForm"
 
-const NewColleauge = () => {
-  const [phoneNumbers, setPhoneNumbers] = useState([{ number: '' }]);
-
-  const [addresses, setAddresses] = useState([{
-    country: '',
-    postalCode: '',
-    city: '',
-    street: '',
-    houseNumber: '',
-    other: ''
-  }])
-
+const NewColleaugePage = () => {
+  const navigate = useNavigate()
+  const [formErrors, setFormErrors] = useState({})
+  const [phoneNumbers, setPhoneNumbers] = useState([{ number: "" }])
+  const [addresses, setAddresses] = useState([
+    {
+      country: "",
+      postalCode: "",
+      city: "",
+      street: "",
+      houseNumber: "",
+      other: ""
+    }
+  ])
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
@@ -23,59 +26,130 @@ const NewColleauge = () => {
     socSecNumber: "",
     tax: "",
     email: "",
-    phoneNumber: phoneNumbers.map(phone => ({ number: phone.number })),
+    phoneNumber: phoneNumbers,
     address: addresses,
     division: ""
   })
 
   const handlePhoneNumberChange = (i, value) => {
-    const newPhoneNumbers = [...phoneNumbers]
-    newPhoneNumbers[i].number = value
+    const newPhoneNumbers = phoneNumbers.map((phone, index) =>
+      index === i ? { ...phone, number: value } : phone
+    )
+
     setPhoneNumbers(newPhoneNumbers)
   }
 
+  const handleAddPhoneNumber = () => {
+    setPhoneNumbers([...phoneNumbers, { number: "" }])
+  }
 
-  const handleInputChange = event => {
-    const { name, value } = event.target;
+  const handleAddressChange = (i, field, value) => {
+    const newAddresses = addresses.map((address, index) =>
+      index === i ? { ...address, [field]: value } : address
+    )
+
+    setAddresses(newAddresses)
+  }
+
+  const handleAddAddress = () => {
+    setAddresses([
+      ...addresses,
+      {
+        country: "",
+        postalCode: "",
+        city: "",
+        street: "",
+        houseNumber: "",
+        other: ""
+      }
+    ])
+  }
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target
+
+    // Email validation
+    if (name === 'email') {
+      const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+      setFormErrors(prevErrors => ({
+        ...prevErrors,
+        email: emailRegex.test(value) ? '' : 'Invalid email format'
+      }))
+    } else {
+      setFormErrors(prevErrors => ({
+        ...prevErrors,
+        [name]: value.trim() ? '' : 'Pls fill out the field'
+      }))
+    }
+
     setFormData(prevData => ({
       ...prevData,
       [name]: value
     }))
   }
 
-  const handleFormSubmit = () => {
-    fetch('/dummyColleguesData.json', {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        "name": `${formData.firstName} ${formData.lastName}`,
-        "email": formData.email,
-        "phoneNumber": formData.phoneNumber,
-        "birthday": formData.birthday,
-        "birthPlace": formData.birthPlace,
-        "motherName": formData.motherName,
-        "socSecNumber": formData.socSecNumber,
-        "tax": formData.tax,
-        "addresses": formData.address,
-        "division": formData.division
-      }),
-    })
-      .then(res => res.json())
-      .then(data => console.log("Success:", data))
-      .catch(err => console.error("Error:", err))
+  /*
+  const handleClearForm = () => {
+    setFormData(initialFormState)
+    setPhoneNumbers([{ number: "" }])
+    setAddresses([
+      {
+        country: "",
+        postalCode: "",
+        city: "",
+        street: "",
+        houseNumber: "",
+        other: ""
+      }
+    ])
+  }
+  */
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault()
+
+    const postData = {
+      ...formData,
+      phoneNumber: phoneNumbers,
+      address: addresses,
+    }
+
+    console.log(postData)
+
+    /*
+   fetch('/api/dummyAPI', {
+     method: "POST",
+     headers: {
+       "Content-Type": "application/json",
+     },
+     body: JSON.stringify(postData),
+   })
+     .then(res => res.json())
+     .then(data => navigate("/colleagues"))
+     .catch(err => console.error("Error:", err))
+     */
+
+    navigate("/colleagues")
   }
 
   return (
-    <div className="new-colleague-field">
+    <form className="new-colleague-field" onSubmit={handleFormSubmit}>
       <div className="container">
         <h2>New Colleague</h2>
-        <NewColleagueForm />
-        <NewColleagueButtons handleFormSubmit={handleFormSubmit} />
+
+        <NewColleagueForm
+          handlePhoneNumberChange={handlePhoneNumberChange}
+          handleAddPhoneNumber={handleAddPhoneNumber}
+          handleAddressChange={handleAddressChange}
+          handleAddAddress={handleAddAddress}
+          formData={formData}
+          handleInputChange={handleInputChange}
+          formErrors={formErrors} />
+        <NewColleagueButtons />
       </div>
-    </div>
+    </form>
   )
 }
 
-export default NewColleauge
+export default NewColleaugePage
